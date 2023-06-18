be4eae3456e6f4718eb1bda4cb2d1f9ee1040728
Ziko67890 <olazikoayobami@gmail.com>
2023-06-18 16:35:22 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = 48; d < 58; d++)
+	{
+	putchar(d);
+	if (d != 57)
+	{
+	putchar(',');
+	putchar(',');
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
