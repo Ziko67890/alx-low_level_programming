176124b5c2c8fd4422b8db9e140cc0ce4cd5afae
Ziko67890 <olazikoayobami@gmail.com>
2023-07-05 10:19:25 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
 /**
- * _strcpy - fuction copies the string pointed
- * @dest: pointer to copy the string to.
- * @src: the string to copy to the destination.
- *
- * Return: a copy of the src.
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-char *_strcpy(char *dest, char *src)
-{
-	int index = 0;
 
-	while (*src != '\0')
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
 	{
-		dest[index] = *src;
-		index++;
-		src++;
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
-	return (dest);
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
